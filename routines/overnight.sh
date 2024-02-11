91a1112554fd9bbb4cfc2a4072f1482a16ed3141
jordannkennedyy <123903208+jordannkennedyy@users.noreply.github.com>
2024-02-10 23:56:12 -0800

    Update overnight.sh

--- routines/overnight.sh
+++ routines/overnight.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# https://crontab.guru/#0_1_7_2_*
+# https://crontab.guru/#0_1_7_2_* or use below
 
+python ./routine_1.py ;
+python ./routine_2.py ;
+python ./routine_3.py ;
+python ./routine_4.py ;
 python ./routine_5.py ;
 
 
